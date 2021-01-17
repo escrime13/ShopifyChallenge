e94e22d19a98fe3671d2e8fccce6758e0a18ad35
escrime13 <55107687+escrime13@users.noreply.github.com>
2021-01-16 20:24:15 -0500

    smaller text done

--- ShopifyChallenge/src/components/Done.jsx
+++ ShopifyChallenge/src/components/Done.jsx
@@ -5,12 +5,12 @@ class UnconnectedDone extends Component {
   render = () => {
       return (
         <div>
-            <h2 class="text-center pt-5">
-                Your are all done! Thanks for nominating 5 movies for The Shoppies Awards!
-            </h2>
             <h3 class="text-center pt-5">
-                You can still edit your choices by removing at least one of the movies you selected.
+                Your are all done! Thanks for nominating 5 movies for The Shoppies Awards!
             </h3>
+            <h4 class="text-center pt-5">
+                You can still edit your choices by removing at least one of the movies you selected.
+            </h4>
         </div>
 
       );
